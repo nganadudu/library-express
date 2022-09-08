34be3c3c3186a3e35fec8f7f6730ae2ffa96c7e8
hblab-ngandt <ngandt@hblab.vn>
2022-09-08 22:58:27 +0700

    Display book detail

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,6 +2,8 @@ const Genre = require('../models/genre');
 const Book = require('../models/book');
 const async = require("async");
 
+const mongoose = require("mongoose");
+
 // Display list of all Genre.
 exports.genre_list = function(req, res, next) {
     Genre.find()
--- models/book.js
+++ models/book.js
@@ -29,7 +29,7 @@ const bookSchema = new Schema(
 );
 
 bookSchema.virtual('url').get(function() {
-    return '/catalog/book/ + this._id';
+    return '/catalog/book/' + this._id;
 })
 
 module.exports = mongoose.model('Book', bookSchema);
